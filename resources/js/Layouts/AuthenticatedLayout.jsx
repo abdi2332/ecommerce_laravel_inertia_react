95ae58ea09334b349b42ed19b4c975e777907954
abdulhadi heyru <101140054+abdiiiiiiiii@users.noreply.github.com>
2024-07-29 00:45:32 +0300

    fixed logout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -71,7 +71,7 @@ export default function Authenticated({ user, header, children }) {
                                 </Dropdown.Trigger>
                                 <Dropdown.Content>
                                     <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
-                                    <Dropdown.Link href="#" onClick={logout}>Log Out</Dropdown.Link>
+                                    <Dropdown.Link href="/logout" method="post">Log Out</Dropdown.Link>
                                 </Dropdown.Content>
                             </Dropdown>
                         </div>
