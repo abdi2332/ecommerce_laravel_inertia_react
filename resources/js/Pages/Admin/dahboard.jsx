9f4b53cdee001a8813a138b132f34db87eba4ea5
abdulhadi heyru <101140054+abdiiiiiiiii@users.noreply.github.com>
2024-08-02 00:13:46 +0300

    admin template integratinf

---
+++ resources/js/Pages/Admin/dahboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dahboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default dahboard
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,21 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
+        <!-- External CSS and JS -->
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('css/tailwind.output.css') }}" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+
+        <!-- External Scripts -->
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+
+        <!-- Local Scripts -->
+        <script src="{{ asset('js/init-alpine.js') }}" defer></script>
+        <script src="{{ asset('js/charts-lines.js') }}" defer></script>
+        <script src="{{ asset('js/charts-pie.js') }}" defer></script>
+
+        <!-- Inertia & Vite -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
